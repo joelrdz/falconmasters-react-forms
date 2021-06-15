7ffa953314a13fc496787954b9301b56256c6cfe
Joel Rdz <joelrdzh@gmail.com>
2021-06-15 16:15:05 -0500

    Project clean, styled components, react-icons, first elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Label, InputGroup, Input, FeedbackError, IconValidation, TermsContainer, CenteredButtonContainer, Button, MessageSuccess, MessageError } from './elements/Forms';
+import { FaCheckCircle, FaTimesCircle, FaExclamationTriangle } from 'react-icons/fa';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Form action="">
+        <div>
+          <Label htmlFor="name">Usuario</Label>
+          <InputGroup>
+            <Input type="text" placeholder="usuario" id="name" />
+            <IconValidation><FaCheckCircle /><FaTimesCircle /></IconValidation>
+          </InputGroup>
+          <FeedbackError>Lorem ipsum</FeedbackError>
+        </div>
+        <TermsContainer>
+          <Label>
+            <input type="checkbox" name="terms" id="terms" />
+            Acepto los Términos y Condiciones
+          </Label>
+        </TermsContainer>
+        {false && <MessageError>
+          <p>
+            <FaExclamationTriangle />
+            <b>Error:</b> Por favor llena el formulario correctamente.
+          </p>
+        </MessageError>}
+        <CenteredButtonContainer>
+          <Button type="submit">Enviar</Button>
+          <MessageSuccess>Formulario enviado exitosamente!</MessageSuccess>
+        </CenteredButtonContainer>
+      </Form>
+    </main>
   );
 }
-
-export default App;
---
+++ src/elements/Forms.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+
+const colors = {
+  border: '#0075ff',
+  error: '#bb2929',
+  success: '#1ed12d',
+}
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const Label = styled.label`
+  display: block;
+  font-weight: 700;
+  padding: 10px;
+  min-height: 40px;
+  cursor: pointer;
+`;
+
+const InputGroup = styled.div`
+  position: relative;
+  z-index: 90;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  background-color: white;
+  border-radius:  3px;
+  height: 45px;
+  line-height: 45px;
+  padding: 0 40px 0 10px;
+  transition: .3s ease all;
+  border: 3px solid transparent;
+
+  &:focus {
+    border-color: ${colors.border};
+    outline: none;
+    box-shadow: 3px 0 30px rgba(163, 163, 163, .4);
+  }
+`;
+
+const FeedbackError = styled.p`
+  font-size: 12px;
+  margin-bottom: 0;
+  color: ${colors.error};
+  display: none;
+`;
+
+const IconValidation = styled.div`
+  position: absolute;
+  right: 10px;
+  bottom: 14px;
+  z-index: 100;
+  font-size: 16px;
+  opacity: 0;
+`;
+
+const TermsContainer = styled.div`
+  grid-column: span 2;
+
+  input {
+    margin-right: 10px;
+  }
+`;
+
+const CenteredButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  grid-column: span 2;
+`;
+
+const Button = styled.button`
+  height: 45px;
+  line-height: 45px ;
+  width: 30%;
+  background-color: black;
+  color: white;
+  font-weight: 700;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: .1s ease all;
+
+  &:hover {
+    box-shadow: 3px 0 30px rgba(163, 163, 163, 1);
+  }
+`;
+
+const MessageSuccess = styled.p`
+  font-size: 14px;
+  color: ${colors.success};
+  display: none;
+`;
+
+const MessageError = styled.div`
+  height: 45px;
+  line-height: 45px;
+  background-color: #f66060;
+  padding: 0 15px;
+  border-radius: 3px;
+  grid-column: span 2;
+
+  p {
+    margin: 0;
+  }
+
+  b {
+    margin-left: 10px;
+  }
+`;
+
+
+export {
+  Form,
+  Label,
+  InputGroup,
+  Input,
+  FeedbackError,
+  IconValidation,
+  TermsContainer,
+  CenteredButtonContainer,
+  Button,
+  MessageSuccess,
+  MessageError
+};
